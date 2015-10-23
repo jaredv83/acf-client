30a9beeb28f97f34b57f46c1c9db525631f057d6
Kai Curry <kai@sundaysenergy.com>
2015-10-23 13:44:43 -0500

    start on member detail page

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { IndexRoute, Route } from 'react-router';
 import {
     App,
     About,
+    Member,
     Members,
     NotFound,
   } from './containers/index';
@@ -19,7 +20,7 @@ export default () => {
       { /* Routes */ }
       <Route path="about" component={About} />
       <Route path="members" component={Members} />
-
+      <Route path="members/:id" component={Member} />
       { /* Catch all route */ }
       <Route path="*" component={NotFound} status={404} />
     </Route>
