cc0a3c204f59e1039cad5b592d969fac30c40fbf
Tim Lupfer <tim@timlupfer.com>
2015-10-23 14:09:43 -0500

    Add image and proptypes for member detail page

--- src/components/Members/MemberDetail.js
+++ src/components/Members/MemberDetail.js
@@ -3,10 +3,14 @@ import React, { Component, PropTypes } from 'react';
 class MembersDetail extends Component {
 
   render() {
-    const {displayName} = this.props;
+    const {displayName, image} = this.props;
     return (
       <div>
         <h1>{displayName}</h1>
+        <img src={image.uri} style={{
+          display: 'inline-block',
+          paddingRight: '10px',
+        }} />
       </div>
     );
   }
@@ -14,6 +18,14 @@ class MembersDetail extends Component {
 
 MembersDetail.propTypes = {
   displayName: PropTypes.string.isRequired,
+  image: PropTypes.shape({
+    uri: PropTypes.string.isRequired,
+    height: PropTypes.number,
+    width: PropTypes.number,
+  }),
+  intro: PropTypes.string,
+  slug: PropTypes.string.isRequired,
+  usState: PropTypes.string,
 };
 
 export default MembersDetail;
