35567ec7b0a1962d6a4bd208ca6ce83b1fc1307b
Kai Curry <kc@Kais-MacBook-Air.local>
2015-10-28 10:30:24 -0500

    fix all the terminal errors

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/server';
 import config from './config';
 import favicon from 'serve-favicon';
 import compression from 'compression';
-import httpProxy from 'http-proxy';
+// import httpProxy from 'http-proxy';
 import path from 'path';
 import createStore from './redux/create';
 import ApiClient from './helpers/ApiClient';
@@ -26,10 +26,10 @@ const pretty = new PrettyError();
 const app = new Express();
 const server = new http.Server(app);
 // Proxy our API server.
-const proxy = httpProxy.createProxyServer({
-  target: 'http://localhost:' + config.apiPort,
-  ws: true, // activate websocket support.
-});
+// const proxy = httpProxy.createProxyServer({
+//   target: 'http://localhost:' + config.apiPort,
+//   ws: true, // activate websocket support.
+// });
 
 app.use(compression());
 app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')));
@@ -37,23 +37,23 @@ app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')));
 app.use(require('serve-static')(path.join(__dirname, '..', 'static')));
 
 // Proxy to API server
-app.use('/api', (req, res) => {
-  proxy.web(req, res);
-});
+// app.use('/api', (req, res) => {
+//   proxy.web(req, res);
+// });
 
 // added the error handling to avoid https://github.com/nodejitsu/node-http-proxy/issues/527
-proxy.on('error', (error, req, res) => {
-  let json;
-  if (error.code !== 'ECONNRESET') {
-    console.error('proxy error', error);
-  }
-  if (!res.headersSent) {
-    res.writeHead(500, {'content-type': 'application/json'});
-  }
-
-  json = {error: 'proxy_error', reason: error.message};
-  res.end(JSON.stringify(json));
-});
+// proxy.on('error', (error, req, res) => {
+//   let json;
+//   if (error.code !== 'ECONNRESET') {
+//     console.error('proxy error', error);
+//   }
+//   if (!res.headersSent) {
+//     res.writeHead(500, {'content-type': 'application/json'});
+//   }
+//
+//   json = {error: 'proxy_error', reason: error.message};
+//   res.end(JSON.stringify(json));
+// });
 
 app.use((req, res) => {
   if (__DEVELOPMENT__) {
