ec7089445543a95ec708d42cf81e08efe789fef0
Kai Curry <kai@cape.io>
2015-10-28 21:27:09 -0500

    prep for deploy

--- src/config.js
+++ src/config.js
@@ -2,11 +2,9 @@ module.exports = {
   development: {
     isProduction: false,
     port: process.env.PORT,
-    apiPort: process.env.APIPORT,
   },
   production: {
     isProduction: true,
     port: process.env.PORT,
-    apiPort: process.env.APIPORT,
   },
 }[process.env.NODE_ENV || 'development'];
