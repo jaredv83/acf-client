2e72e5b79010668eaf3e7df0f91b9875a45fcc6b
Kai Curry <kai@cape.io>
2015-11-13 10:44:33 -0600

    put about text in database somehow later

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -11,6 +11,15 @@ export default class About extends Component {
 
         <p>The American Composers Forum enriches lives by nurturing the creative spirit of composers and communities. We provide new opportunities for composers and their music to flourish, and engage communities in the creation, performance and enjoyment of new music.</p>
 
+        <h2>History</h2>
+
+        <p>The American Composers Forum is committed to supporting composers and developing new markets for their music. Through granting, commissioning, and performance programs, the Forum provides composers at all stages of their careers with valuable resources for professional and artistic development. By linking communities with composers and performers, the Forum fosters a demand for new music, enriches communities, and helps develop the next generation of composers, musicians, and music patrons. </p>
+        <p>Founded in 1973 as the Minnesota Composers Forum, the organization has grown from an innovative regional initiative into one of the nation's premier composer service organizations. Forum programming reaches composers and communities in all 50 states.</p>
+        <p>The Forum helps composers engage communities with music as a source of inspiration, self-reflection and delight. This engagement takes the form of groundbreaking composer residencies, designed to engage communities in the creative process and broaden the contexts in which new music is written, performed and heard. It means innovative approaches to teaching music while nurturing the next generation of composers, performers and audiences.</p>
+        <p>{`The Forum supports composers' artistic and professional growth through a rich variety of programs and services, including commissions, performances, readings and fellowships.`}</p>
+        <p>The Forum's 2,000 members include composers and performers, presenters and organizations that share the Forum's goals, and individuals and institutions with an interest in supporting new music. Members come from both urban and rural areas; they work in virtually every musical genre, including orchestral and chamber music, "world" music, opera and music theater, jazz and improvisational music, electronic and electro-acoustic music, and sound art. </p>
+        <p>In addition to the tangible benefits of membership (newsletters, invitations to events, and access to grant, fellowship, and residency opportunities), members are part of a national community of artists who share common concerns, aspirations, and goals.</p>
+        <p></p>
       </div>
     );
   }
