b6b18961f38764f6000428e66e1e1d48a1a29870
Kai Curry <kai@cape.io>
2015-11-13 12:38:54 -0600

    pager the members

--- src/containers/members.js
+++ src/containers/members.js
@@ -1,10 +1,15 @@
 import { connect } from 'react-redux';
 import Component from 'components/Members/Members';
+import { getPagerInfo } from '../helpers/pager';
 
-
-function mapStateToProps({members: {data}}) {
+function mapStateToProps({members: {data}, router: { location: { query } }}) {
+  const page = query && query.page;
+  // Filter down the members.
+  const {list, hasMore, hasLess} = getPagerInfo(data, {pageIndex: page});
   return {
-    members: data,
+    members: list,
+    hasLess,
+    hasMore,
   };
 }
 
---
+++ src/helpers/pager.js
@@ -0,0 +1,22 @@
+export function getPagerInfo(items, opts) {
+  const defaultOpts = {
+    page: 1,
+    perPage: 48,
+  };
+  const { page, perPage } = {...defaultOpts, ...opts};
+  const totalItems = items.length;
+  const maxPage = Math.ceil(totalItems / perPage);
+  const pageIndex = page < maxPage ? page : maxPage;
+  const itemsStart = (pageIndex - 1) * perPage;
+  const itemsEnd = itemsStart + perPage;
+  const list = items.slice(itemsStart, itemsEnd);
+  return {
+    hasLess: pageIndex > 1,
+    hasMore: pageIndex < maxPage,
+    list,
+    maxPage,
+    pageIndex,
+    perPage,
+    totalItems,
+  };
+}
