91c8aa9afeecd6022ec55b704c091bb08a91f559
Kai Curry <kai@cape.io>
2015-11-16 11:06:35 -0600

    send down pageIndex

--- src/containers/members.js
+++ src/containers/members.js
@@ -5,14 +5,17 @@ import { getPagerInfo } from '../helpers/pager';
 function mapStateToProps({members: {data}, router: { location: { query } }}) {
   const page = query && parseInt(query.page, 10);
   // Filter down the members.
-  const {list, hasMore, hasLess} = getPagerInfo(data, {page});
+  const {list, hasMore, hasLess, pageIndex } = getPagerInfo(data, {page});
   return {
     members: list,
     hasLess,
     hasMore,
+    pageIndex,
   };
 }
 
-// const mapDispatchToProps = {};
+// const mapDispatchToProps = {
+//   handleNext:
+// };
 
 export default connect(mapStateToProps)(Component);
