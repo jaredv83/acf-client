57c7670ac2020258c6490297590c03f68b75830c
Tim Lupfer <tim@timlupfer.com>
2015-11-16 11:15:46 -0600

    Add pager functionality to members component TODO: move page to its own component?

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
 import DocumentMeta from 'react-document-meta';
+import classnames from 'classnames';
 import MembersPreview from './MembersPreview';
 
 class Members extends Component {
 
   render() {
-    const { members } = this.props;
+    const { members, hasLess, hasMore, pageIndex } = this.props;
 
     return (
       <div className="container">
@@ -20,12 +22,50 @@ class Members extends Component {
           })
         }
         </div>
+        <nav>
+          <ul className="pager">
+            <li className={classnames({
+              previous: true,
+              disabled: !hasLess,
+            })}>
+              <Link
+                to={`/members?page=${pageIndex - 1}`}
+                onClick={(evt) => {
+                  if (!hasLess) {
+                    evt.preventDefault();
+                    return false;
+                  }
+                }}
+              >
+                Previous
+              </Link>
+            </li>
+            <li className={classnames({
+              next: true,
+              disabled: !hasMore,
+            })}>
+              <Link
+                to={`/members?page=${pageIndex + 1}`}
+                onClick={(evt) => {
+                  if (!hasMore) {
+                    evt.preventDefault();
+                    return false;
+                  }
+                }}
+              >
+                Next
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
 }
 
 Members.propTypes = {
+  hasLess: React.PropTypes.bool,
+  hasMore: React.PropTypes.bool,
   members: React.PropTypes.array,
 };
 
